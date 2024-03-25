becd18f19c2a05bba0dfb946c58a8d750018fb74
malinhodev <malinhodev@gmail.com>
2024-03-25 13:37:19 -0300

    aprendendo sobre variaveis e tipos em python

---
+++ teste.py
@@ -0,0 +1,22 @@
+#numeros
+'''
+inteiro = 1
+fracional = 1.5
+equacional = 1+2j
+
+a = "hello"
+b = " word!"
+#concatenação
+c = a + b
+
+
+
+print(c)'''
+
+#casting
+
+a = int(2)
+b = int(2.8)
+c = int('2')
+
+print(a, b, c)
