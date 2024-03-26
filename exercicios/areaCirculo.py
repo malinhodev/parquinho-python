dce0eb9b8f6cafd0b75aa81aef1dead9da90b09d
malinhodev <malinhodev@gmail.com>
2024-03-26 10:30:03 -0300

    criando algoritimo que calcula a area de um circulo

---
+++ exercicios/areaCirculo.py
@@ -0,0 +1,5 @@
+print('vamos calcular a area do circulo.')
+raio = input('qual é o raio do circulo? ')
+pi = 3.14
+area = float(pi) * float(raio)**2
+print(f'a area do circulo é aproximadamente: {area} unidade de medida.')
