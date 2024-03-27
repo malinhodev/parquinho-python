645701f459c8f7c26dbdadcba4dc9f4bc22ff5e9
malinhodev <malinhodev@gmail.com>
2024-03-27 12:39:05 -0300

    aprendendo a criar agoritimo que diz se um numero é positivo ou negativo

---
+++ verfica_sinal.py
@@ -0,0 +1,9 @@
+
+numero = int(input('digite um numero: '))
+
+if numero > 0:
+    print(f'o numero {numero} é positivo!')
+elif numero < 0:
+    print(f'o numero {numero} é negativo!')
+else:
+    print('digite um numero!')
