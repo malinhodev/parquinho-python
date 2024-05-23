edd46229bea05d62e23fed9cb886fea92890bd90
malinhodev <malinhodev@gmail.com>
2024-05-23 00:53:48 -0300

    criando algoritimo de calculo do imc  de uma  pessoa

---
+++ exercicios/imc.py
@@ -0,0 +1,5 @@
+print('vamos calcular seu IMC.')
+peso = input('qual é o seu peso? ')
+altura = input('qual é sua altura? ')
+imc = float(peso) / float(altura)**2
+print(f'O calculo do seu IMC é aproximadamente: {imc}.')
