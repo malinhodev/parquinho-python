9c77a8afbf3eb2a1e2c91787ff7f15a79d24988c
malinhodev <malinhodev@gmail.com>
2024-05-24 09:57:22 -0300

    criando algoritimo para descobrir se um numero é impa ou par

---
+++ exercicios e  algoritimos/impaoupar.py
@@ -0,0 +1,10 @@
+print(30 * '-')
+print("vamos descobrir se o número é  pá ou ímpar ")
+num = int(input('insira um número '))
+
+if (num % 2) == 0:
+    print(f'o numero {num} é par!')
+else:
+    print(f'o numero {num} é ímpar!')
+
+print(30 * '-')
