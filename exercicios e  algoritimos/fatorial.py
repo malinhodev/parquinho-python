3d5147ada6e8358f0c035035081f6b05c7f51a5b
malinhodev <malinhodev@gmail.com>
2024-05-24 10:24:39 -0300

    criando algoritimo de  calulo  de fatorial

---
+++ exercicios e  algoritimos/fatorial.py
@@ -0,0 +1,25 @@
+"""
+como achar o fatorial de um numero
+
+fatorial?
+9! = 9*8*7*6*5*4*3*2*1
+4! = 4*3*2*1
+0! = 1
+1! = 1
+"""
+print(30 * '-')
+print('vamos avhar o  fatorial do numero')
+numero = int(input('insira um numero: '))
+
+Fatorial = 1
+
+if numero < 0:
+    print('Não existe fatorial de numeros neagtivos')
+elif numero == 0:
+    print(f'O fatorial de {numero} é 1')
+else:
+    for x in range(1, numero+1):
+        Fatorial = Fatorial*x
+    print(f'O fatorial de {numero} é {Fatorial}')
+
+print(30 * '-')
